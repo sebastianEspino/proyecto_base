f8320c407421967393e4d92ba305ee7cd56c2b23
sebastianEspino <sebasespinosa2016@gmail.com>
2023-11-08 11:35:56 -0500

    Inicio commit

---
+++ android/app/src/main/kotlin/com/example/proyecto_base/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.proyecto_base
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'app/app.dart';
 
-void  main(){
+void main() {
   runApp(const App());
 }
+
